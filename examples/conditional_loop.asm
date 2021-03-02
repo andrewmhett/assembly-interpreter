09f448d5e4bd1e0a2a15d99e1feba1849651be9c
Andrew Hett <andrewmhett@gmail.com>
2021-03-02 15:35:32 -0800

    Updated conditional loop example

--- examples/conditional_loop.asm
+++ examples/conditional_loop.asm
@@ -10,6 +10,14 @@ add 3 c
 msg "reg a: " a
 msg "reg b: " b
 msg "reg c: " c
+cmp a 5
+jgt a_greater_than_five
 cmp a 10
 jlt top
+jet bottom
+a_greater_than_five:
+msg "reg a is greater than 5"
+cmp a 10
+jlt top
+bottom:
 msg "Finished looping."
